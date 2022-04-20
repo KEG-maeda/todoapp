9b4a57cec6f2b358bc865c8b2a180c46fc222630
KEG-maeda <k.maeda@g.dreamcareer.co.jp>
2022-04-20 19:03:35 +0900

    登録フォームリニューアル。全体的なデザインもリニューアル。新しい更新フォームは未完成

--- src/tasks/list.js
+++ src/tasks/list.js
@@ -14,7 +14,7 @@ listTasks = async function () {
     try {
         connection = await mysql.createConnection(config.dbSetting);//config.jsにあるdbSettingオブジェクトを入れる
                                    //↑↑これでDBに接続する。これもお決まりお守り。mysql.createConnectionの仕様は調べて
-        const sql = "SELECT `id`, `task_name`, `deadline`, `category_id` FROM `todoapp`.`t_task`;";
+        const sql = "SELECT `id`, `task_name`, `deadline`, `category_id`, `task_status` FROM `todoapp`.`t_task`;";
         const [rows, fields] = await connection.query(sql);
 
         return rows;//[api/index.js] ->
